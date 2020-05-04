a2317633a5501b536efc810adc5b619802f83bcb
Daniel Villanueva <daniel@Daniels-MacBook-Pro.local>
2020-05-04 18:24:30 +0700

    intial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+function calculateTip() {
+  var billAmount = document.getElementById("amount").value;
+  var service = document.getElementById("service").value;
+  var numPeople = document.getElementById("people").value;
+  var finalTip = document.getElementById("finalTip");
+  var amountTip = document.getElementById("tip");
+
+  var finalTotal = document.getElementById("finalTotal");
+  var billTotal = document.getElementById("total");
+
+  if (billAmount <= 0 || billAmount === "" || numPeople === 0) {
+    alert("Please enter valid values!");
+    return;
+  }
+
+  var totalTip = billAmount * service;
+  var total = Number(billAmount) + totalTip;
+
+  if (numPeople == "1") {
+    amountTip.innerHTML = totalTip.toFixed(2);
+  } else {
+    totalTip = totalTip / numPeople;
+    var total = Number(billAmount) + totalTip;
+    amountTip.innerHTML = totalTip.toFixed(2);
+    document.getElementById("each").display = "inline";
+    document.getElementById("each2").display = "inline";
+  }
+
+  billTotal.innerHTML = total.toFixed(2);
+  finalTotal.style.display = "block";
+  finalTip.style.display = "block"
+}
+
+document.getElementById("finalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+document.getElementById("each2").style.display = "none";
+document.getElementById("finalTotal").style.display = "none";
+document.getElementById("calculate").onclick = function () {
+  calculateTip();
+};
